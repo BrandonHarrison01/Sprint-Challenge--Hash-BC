fa68073aa5816210f07d0ef52b58c361f66615bd
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-12-06 09:51:37 -0700

    miner is mining .... I think

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -2,6 +2,7 @@ import hashlib
 import requests
 
 import sys
+import json
 
 from uuid import uuid4
 
@@ -23,8 +24,11 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    last_hash = json.dumps(last_proof, sort_keys=True)
+    proof = 1
     #  TODO: Your code here
+    while valid_proof(last_hash, proof) is False:
+        proof *= 7
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +44,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{last_hash}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
